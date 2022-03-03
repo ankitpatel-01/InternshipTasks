ad1cf9ee1571a2d4c67b80fc043250a446dad172
Ankitkumar Patel <ankitkumar.patel@1Rintern.local>
2022-03-03 17:56:46 +0530

    bootstrap

---
+++ JS/assets/js/script.js
@@ -0,0 +1,31 @@
+
+// Array methods
+const interns = [
+    {
+        id:1,
+        name:"Ankit Patel",
+        company:"1Rivet"
+    },
+    {
+        id:2,
+        name:"Aman Patel",
+        company:"1Rivet"
+    },
+    {
+        id:3,
+        name:"Jay Patel",
+        company:"gcvlc"
+    },
+    {
+        id:4,
+        name:"Harsh Singh",
+        company:"Enjay"
+    },
+    {
+        id:5,
+        name:"Amit",
+        company:"google"
+    },
+];
+
+console.log(interns);
